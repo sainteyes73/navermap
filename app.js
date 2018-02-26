a06a90c1ff58b18e399691f015a4572042e07442
woosung <sainteyes73@gmail.com>
2018-02-27 00:33:39 +0900

    favicon

--- app.js
+++ app.js
@@ -42,10 +42,8 @@ models.forEach(function (model) {
   require(model);
 });
 
-
-module.exports = require('./config/express')(app, config);
-
-
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
+
+module.exports = require('./config/express')(app, config);
